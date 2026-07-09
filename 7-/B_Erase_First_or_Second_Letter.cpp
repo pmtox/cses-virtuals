ebb449829cdb67b5b4920cbacf553b3b25e32bb1
pmtox <pmishraa24@gmail.com>
2026-07-09 17:32:12 +0530

    added

--- 7-/B_Erase_First_or_Second_Letter.cpp
+++ 7-/B_Erase_First_or_Second_Letter.cpp
@@ -1,63 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long int
-// bool fact(string s){
-//     unordered_map<char , int> mpp;
-//     for(int i : s){
-//         mpp[i]++;
-//     }
-//     for(auto it : mpp){
-//         if(mpp.size() == 1 && it.second == s.size()){
-//             return true;
-//         }
-//     }
-//     return false;
-// }
+// we can count number of characters;
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL);
 void solve(){
     int n;cin>>n;
     string s;cin>>s;
-    // if(fact(s)){
-    //     cout<<n<<endl;
-    // }
-    // else{
-        map<char , int>mpp;
-        vector<int>arr(n , 0);
-        int cnt = 0;
-        for(int i = 0 ; i < n ; i++){
-            mpp[s[i]]++;
-            if(mpp[s[i]] == 1){
-                cnt++;
-            }
-            arr[i] = cnt;
+    int cnt = 0;
+    map<char , int>mpp;
+    vector<int>arr(n , 0);
+    // cout<<(a - (t))<<" :: ";
+    for(int i = 0 ; i < n ; i++){
+        mpp[s[i]]++;
+        if(mpp[s[i]] == 1){
+            cnt++;
+            // cout<<"cnt = "<<cnt<<" ";
+            // cout<<"MPP = "<<mpp[s[i]]<<" ";
         }
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            ans += arr[i];
-        }
-        cout<<ans<<endl;
+        arr[i] = cnt;
+        // cout<<arr[i]<<" ";
+    }
+    int ans = 0;
+    for(int i = 0 ; i < n ; i++){
+        ans += arr[i];
+    }
+    // for(auto &i : mpp){
+    //     cout<<i.first<<" "<<"-> "<<i.second;
+    //     cout<<endl;
     // }
+    // cout<<endl;
+    cout<<ans<<endl;
 }
 void ansss(){
     int n;cin>>n;
     string s;cin>>s;
-    int ttlAns = 0;
     set<char>st;
     st.insert(s.begin() , s.end());
-    int lenst = st.size();
     map<char , int>mpp;
     for(int i = 0 ; i < n ; i++){
         mpp[s[i]]++;
     }
+    int ttl = 0;
     for(int i = n ; i >= 0 ; i--){
         if(!(--mpp[s[i]])){
             st.erase(s[i]);
         }
-        ttlAns += st.size();
+        ttl += st.size();
     }
-    cout<<ttlAns<<endl;
+    cout<<ttl<<endl;
 }
 int32_t main(){
     int t;cin>>t;
+    // int a = t;
     while(t--){
         // solve();
         ansss();

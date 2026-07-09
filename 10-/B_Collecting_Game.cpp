63a3b4fab5dc6e7c5119a19f486df1f2f3a7b668
pmtox <pmishraa24@gmail.com>
2026-07-10 02:33:24 +0530

    added

--- 10-/B_Collecting_Game.cpp
+++ 10-/B_Collecting_Game.cpp
@@ -1,27 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long int
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL);
 void solve(){
-    int n ; cin>>n;
-    vector<pair<int , int>>vp;
-    // {to maintain order we used vector pair}
+    int n;cin>>n;
+    vector<pair<int , int>>vp;  // don't allocate when doing push_back
+    // ends up taking 2n size;! LOL
     for(int i = 0 ; i < n ; i++){
-        int x;cin>>x;
+        int x ; cin>>x;
         vp.push_back({x , i});
     }
     sort(vp.begin() , vp.end());
-    vector<int>pref(n , 0);
-    pref[0] = vp[0].first;
+    vector<int>pre(n , 0);
+    pre[0] = vp[0].first;
     for(int i = 1 ; i < n ; i++){
-        pref[i] = pref[i - 1] + vp[i].first; 
+        pre[i] = pre[i-1] + vp[i].first;
     }
     vector<int>ans(n);
     for(int i = 0 ; i < n ; i++){
         int j = i;
         int found = i;
         while(j < n){
-            pair<int , int> tmp = {pref[j] + 1 , INT_MIN};
-            int idx = lower_bound(vp.begin() , vp.end() , tmp) - vp.begin();
+            pair<int , int> prr = {pre[j] + 1 , INT_MIN};
+            int idx = lower_bound(vp.begin() , vp.end() , prr) - vp.begin();
+            // subtracted coz we took 1 extra index in tmp pair;
             idx--;
             if(idx == j){
                 break;

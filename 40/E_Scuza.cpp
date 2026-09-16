ec8dd18aee9e141a29c680771f771a4493804206
pmtox <pmishraa24@gmail.com>
2026-09-17 02:42:19 +0530

    added

--- 40/E_Scuza.cpp
+++ 40/E_Scuza.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define IOS ios::sync_with_stdio(false);cin.tie(nullptr);
-ll bin(vector<ll>&prefMax , ll val , ll n){
+#define ll long long 
+#define IOS ios::sync_with_stdio(false); cin.tie(nullptr);
+ll binaryS(ll k , vector<ll>&prefMax , ll n){
     ll l = 0 , r = n - 1;
     ll ans = -1;
     while(l <= r){
         ll mid = l + (r - l) / 2;
-        if(prefMax[mid] <= val){
+        if(prefMax[mid] <= k){
             ans = mid;
             l = mid + 1;
         }
@@ -18,39 +18,39 @@ ll bin(vector<ll>&prefMax , ll val , ll n){
     return ans;
 }
 void solve(){
-    ll n , q;cin>>n>>q;
+    ll n , m ; cin>>n>>m;
     vector<ll>a(n);
-    vector<ll>k(q);
+    vector<ll>b(m);
     for(ll i = 0 ; i < n ; i++){
         cin>>a[i];
     }
-    for(ll i = 0 ; i < q ; i++){
-        cin>>k[i];
+    for(ll i = 0 ; i < m ; i++){
+        cin>>b[i];
     }
+    // 1 2 1 5
+    // 1 2 4 9 10
     vector<ll>prefSum(n) , prefMax(n);
-    prefSum[0] = a[0];
     prefMax[0] = a[0];
+    prefSum[0] = a[0];
     for(ll i = 1 ; i < n ; i++){
-        prefMax[i] = max(prefMax[i - 1] , a[i]);
         prefSum[i] = prefSum[i - 1] + a[i];
+        prefMax[i] = max(prefMax[i - 1] , a[i]);
     }
-    for(ll i = 0 ; i < q ; i++){
-        ll val = k[i];
-        ll ind = bin(prefMax , val , n);
-
-        if(ind == -1){
+    for(ll i = 0 ; i < m ; i++){
+        ll k = b[i];
+        ll idx = binaryS(k , prefMax , n);
+        if(idx == -1){
             cout<<0<<" ";
         }
         else{
-            cout<<prefSum[ind]<<" ";
+            cout<<prefSum[idx]<<" ";
         }
     }
     cout<<endl;
 }
 int32_t main(){
     IOS;
-    ll t;cin>>t;
-    while(t--){
+    ll t;cin>>t;while(t--){
         solve();
     }
     return 0;
